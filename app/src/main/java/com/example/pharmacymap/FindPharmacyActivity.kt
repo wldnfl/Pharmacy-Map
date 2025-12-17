3ecd2be4bfaf50eca190b577711f9a0af7d3cd2b
jiwoo <1114jiwoo@gmail.com>
2025-12-17 17:13:29 +0900

    [feat] 약국 찾기 버튼을 통한 페이지 이동 구현

---
+++ app/src/main/java/com/example/pharmacymap/FindPharmacyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pharmacymap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class FindPharmacyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.find_pharmacy)
+    }
+}
--- app/src/main/java/com/example/pharmacymap/MainActivity.kt
+++ app/src/main/java/com/example/pharmacymap/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.pharmacymap
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFindPharmacy = findViewById<Button>(R.id.btnFindPharmacy)
+
+        btnFindPharmacy.setOnClickListener {
+            val intent = Intent(this, FindPharmacyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
